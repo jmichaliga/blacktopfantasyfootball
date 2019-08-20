6b9c14146ba17aed605e688d1b607ed0a7d4ccf9
Justin Michaliga <justin.michaliga@gmail.com>
2019-08-20 10:07:34 -0400

    week 3: set in stone

--- consts/index.js
+++ consts/index.js
@@ -41,3 +41,33 @@ export const beckerRoster = [
   'GHOST 1',
   'GHOST 2'
 ]
+
+export const roster2019 = [{"name":"FREY","team1":"NO","team2":"WAS","score1":0,"score2":0,"img1":"./static/NO.gif","img2":"./static/WAS.gif"}, 
+{"name":"deRUBIO","team1":"JAC","team2":"LA","score1":0,"score2":0,"img1":"./static/JAC.gif","img2":"./static/LA.gif"}, 
+{"name":"DEUTSHE","team1":"KC","team2":"CAR","score1":0,"score2":0,"img1":"./static/KC.gif","img2":"./static/CAR.gif"}, 
+{"name":"BECKER","team1":"SEA","team2":"ATL","score1":0,"score2":0,"img1":"./static/SEA.gif","img2":"./static/ATL.gif"}, 
+{"name":"HICKS","team1":"PIT","team2":"CIN","score1":0,"score2":0,"img1":"./static/PIT.gif","img2":"./static/CIN.gif"}, 
+{"name":"KAZIN","team1":"NYG","team2":"DAL","score1":0,"score2":0,"img1":"./static/NYG.gif","img2":"./static/DAL.gif"}, 
+{"name":"GLANZER","team1":"BAL","team2":"OAK","score1":0,"score2":0,"img1":"./static/BAL.gif","img2":"./static/OAK.gif"}, 
+{"name":"PROBERT","team1":"HOU","team2":"GB","score1":0,"score2":0,"img1":"./static/HOU.gif","img2":"./static/GB.gif"}, 
+{"name":"PERRAS","team1":"NYJ","team2":"DET","score1":0,"score2":0,"img1":"./static/NYJ.gif","img2":"./static/DET.gif"}, 
+{"name":"TAGS","team1":"BUF","team2":"PHI","score1":0,"score2":0,"img1":"./static/BUF.gif","img2":"./static/PHI.gif"}, 
+{"name":"MICHALIGA","team1":"IND","team2":"TEN","score1":0,"score2":0,"img1":"./static/IND.gif","img2":"./static/TEN.gif"}, 
+{"name":"POPACK","team1":"DEN","team2":"NE","score1":0,"score2":0,"img1":"./static/DEN.gif","img2":"./static/NE.gif"}, 
+{"name":"WELCH","team1":"TB","team2":"LAC","score1":0,"score2":0,"img1":"./static/TB.gif","img2":"./static/LAC.gif"}, 
+{"name":"BARCH","team1":"CHI","team2":"CLE","score1":0,"score2":0,"img1":"./static/CHI.gif","img2":"./static/CLE.gif"}]
+
+export const becker2019 = [{"name":"MAX","team1":"LA","team2":"CIN","score1":0,"score2":0,"img1":"../static/LA.gif","img2":"../static/CIN.gif"}, 
+{"name":"GHOST 1","team1":"NYJ","team2":"BAL","score1":0,"score2":0,"img1":"../static/NYJ.gif","img2":"../static/BAL.gif"}, 
+{"name":"ZOE","team1":"MIN","team2":"ARI","score1":0,"score2":0,"img1":"../static/MIN.gif","img2":"../static/ARI.gif"}, 
+{"name":"JAY","team1":"OAK","team2":"ATL","score1":0,"score2":0,"img1":"../static/OAK.gif","img2":"../static/ATL.gif"}, 
+{"name":"MILES","team1":"CAR","team2":"WAS","score1":0,"score2":0,"img1":"../static/CAR.gif","img2":"../static/WAS.gif"}, 
+{"name":"LEE","team1":"CHI","team2":"KC","score1":0,"score2":0,"img1":"../static/CHI.gif","img2":"../static/KC.gif"}, 
+{"name":"GHOST 2","team1":"DET","team2":"GB","score1":0,"score2":0,"img1":"../static/DET.gif","img2":"../static/GB.gif"}, 
+{"name":"FREDDY","team1":"BUF","team2":"JAC","score1":0,"score2":0,"img1":"../static/BUF.gif","img2":"../static/JAC.gif"}, 
+{"name":"PIERCE","team1":"PHI","team2":"SEA","score1":0,"score2":0,"img1":"../static/PHI.gif","img2":"../static/SEA.gif"}, 
+{"name":"JOSH","team1":"CLE","team2":"NYG","score1":0,"score2":0,"img1":"../static/CLE.gif","img2":"../static/NYG.gif"}, 
+{"name":"BEN","team1":"NO","team2":"HOU","score1":0,"score2":0,"img1":"../static/NO.gif","img2":"../static/HOU.gif"}, 
+{"name":"ANDY","team1":"NE","team2":"IND","score1":0,"score2":0,"img1":"../static/NE.gif","img2":"../static/IND.gif"}, 
+{"name":"STEVE","team1":"TEN","team2":"SF","score1":0,"score2":0,"img1":"../static/TEN.gif","img2":"../static/SF.gif"}, 
+{"name":"LISA","team1":"DAL","team2":"LAC","score1":0,"score2":0,"img1":"../static/DAL.gif","img2":"../static/LAC.gif"}]
---
+++ pages/2019.js
@@ -0,0 +1,59 @@
+import react from 'react'
+import _ from 'lodash'
+import axios from 'axios'
+import styles from '../components/styles'
+import Lister from '../components/Lister'
+import { assignScores } from '../utils'
+import { roster2019 } from '../consts'
+
+let results = []
+let scores = []
+let week = 0
+
+const assignTeams = () => {
+  results = roster2019
+  console.log('scores:', scores)
+  if (week === 3) {
+    return assignScores(scores, results)
+  }
+}
+
+class Index extends react.Component {
+  constructor(){
+    super()
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount () {
+    // const unsecure = 'http://www.nfl.com/liveupdate/scores/scores.json'
+    const secure = 'https://feeds.nfl.com/feeds-rs/scores.json'
+    axios.get(secure)
+      .then( res => {
+        return res
+      })
+      .then(parsed => {
+        scores = parsed.data.gameScores
+        week = parsed.data.week
+        assignTeams()
+        this.setState({ loading: false })
+      })
+  }
+
+  render () {
+    return (
+      <div style={styles.container}>
+        <h1 style={styles.h1}>BTFF 2019 Draft Order</h1>
+        <Lister results={results} />
+        <h4>Draft Results are determined by the 2019 Week 3 Preseason Game.</h4>
+        <p style={styles.p}>By the selection above, the total number of points scored by those two teams combined will rank the players in order of which they will make their selections.</p>
+        <p style={styles.p}>If by chance there are any ties, those within the tie have the opportunity to resolve in any creative way possible.</p>
+        <p style={styles.p}>If this doesn't come to a resolution, another random ranking will determine those slots.</p>
+      </div>
+    )
+  }
+
+}
+
+export default Index 
